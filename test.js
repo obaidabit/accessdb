aecac2581de02334eeab2cec63e399d341ee02af
Obedabit <obitbit30@gmail.com>
2020-10-13 23:26:46 +0300

    main query on access file

--- test.js
+++ test.js
@@ -1,19 +1,33 @@
 const ADODB = require("node-adodb");
+const fs = require('fs');
 
 const connection = ADODB.open(
-    "Provider=Microsoft.ACE.OLEDB.16.0;Data Source=alwan.accdb;",
-    true
+    "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=alwan.accdb;"
 );
 
 async function query() {
     try {
         const d = new Date();
         const result = await connection.query(
-            `select * from فواتير_المبيع where معرف_المادة in(select First(معرف_المادة) from مواد_مصنعة where inStr(الاسم&" "&الصنف&" "&النوع,'دفتر راصور بلاستيك عربي وسط 70'))`
+            `SELECT فواتير_الشراء.معرف_البيان, First(فواتير_الشراء.رقم_الفاتورة) AS الفاتورة, First(فواتير_الشراء.التاريخ) AS تاريخ, First(دفعات_موردين.القيمة) AS دفعة, Count(فواتير_الشراء.معرف_الفاتورة) AS عدد_الأقلام FROM الموردون RIGHT JOIN (دفعات_موردين RIGHT JOIN فواتير_الشراء ON دفعات_موردين.معرف_البيان = فواتير_الشراء.معرف_البيان) ON الموردون.معرف_المورد = فواتير_الشراء.معرف_المورد GROUP BY فواتير_الشراء.معرف_البيان ORDER BY First(فواتير_الشراء.التاريخ);`
         );
-        console.log(result);
+        
+		fs.writeFile('data.json',JSON.stringify(result,null,2),err =>{
+			if(err) console.err(err);
+		});
+		/*
+		result.forEach(item =>{
+			fs.writeFile('data.json',item['التاريخ'],err =>{
+				if(err) console.err(err);
+			});
+		}) 
+		*/
+		
     } catch (error) {
         console.error(error);
+		fs.writeFile('logs.txt',error.process.message,(err)=>{
+			if (err) console.log(err)
+		})
     }
 }
 
